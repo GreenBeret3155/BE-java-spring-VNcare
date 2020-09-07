e7878226d70ee338ad61b291870f4a548e6bd641
anhviet9819 <60902057+anhviet9819@users.noreply.github.com>
2020-09-07 16:52:02 +0700

    Update IDangKyKhamService.java

--- src/main/java/com/example/intern/service/IDangKyKhamService.java
+++ src/main/java/com/example/intern/service/IDangKyKhamService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.example.intern.model.DangKyKham;
 
 public interface IDangKyKhamService {
-	List<DangKyKham> queryQuery(Date thoigiandkBegin, Date thoigiandkEnd, Date thoigiankhamBegin, Date thoigiankhamEnd, boolean trangthaikham, Long benhnhanId, Long bacsiId);
+	List<DangKyKham> queryQuery(Date thoigiandkBegin, Date thoigiandkEnd, Date thoigiankhamBegin, Date thoigiankhamEnd, Boolean trangthaikham, Long benhnhanId, Long bacsiId);
 	DangKyKham getOneById(Long id);
 	DangKyKham save(DangKyKham dangkykham);
 	void delete(Long id);
